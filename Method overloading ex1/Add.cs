7c07d24d0975670e84856643a68dfcd4e024bd4e
Amir Gugssa <amirgugssa@gmail.com>
2022-09-26 16:28:23 -0400

    ex done

---
+++ Method overloading ex1/Add.cs
@@ -0,0 +1,56 @@
+﻿namespace Method_overloading_ex1
+{
+    internal class Add
+    {
+        public static int Adds (int x, int y)
+        {
+            return x + y;
+        }
+
+        public static decimal Adds (decimal x, decimal y)
+        {
+            var sum = x + y;
+            return sum;
+        }
+
+        public static string Adds(int x, int y, bool z)
+        {
+            var sum = x + y;
+
+            if (z == true && sum > 1)
+            {
+                return $"{sum} dollars";
+            }
+            else if (z == true && sum == 1)
+            {
+                return $"{sum} dollar";
+            }
+            else if (z == true && sum < 1)
+            {
+                return $"{sum} dollars";
+            }
+            else
+            {
+                return sum.ToString();
+            }
+
+
+            //while (z == true)
+            //{
+            //    if (x + y == 1)
+            //    {
+            //        return $"{sum} dollar";
+            //    }
+            //    else
+            //    {
+            //        return $"{sum} dollar";
+            //    }
+            //    break;
+            //}
+
+        }
+
+
+    }
+}
+
---
+++ Method overloading ex1/Program.cs
@@ -0,0 +1,24 @@
+﻿using Method_overloading_ex1;
+
+var x = 0;
+var y = 1;
+
+
+var addAnswer = Add.Adds(x, y, true);
+
+var a = 22.1m;
+var b = 33.6m;
+
+var decimalAnswer = Add.Adds(a, b);
+
+Console.WriteLine($"{addAnswer}");
+
+if (decimalAnswer > 1 || decimalAnswer < 1)
+{
+    Console.WriteLine($"{decimalAnswer} dollars");   
+}
+else
+{
+    Console.WriteLine($"{decimalAnswer} dollar");
+}
+;
